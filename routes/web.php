e2063187561584cada15f8917a431cb535b2d27d
Umerr-Bashir <112628338+Umerr-Bashir@users.noreply.github.com>
2024-04-16 16:33:31 +0500

    .

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\CustomerController;
 
 Route::get('/', function () {
     return view('admin.test.create');
@@ -14,17 +15,20 @@ Auth::routes();
 Route::group(['middleware' => ['auth']], function () {
 
     Route::group(['prefix' => 'customers'], function () {
-    Route::get('', [CustomerController::class, 'index'])->name('customers.index');
-    Route::post('update', [CustomerController::class, 'update'])->name('customers.update');
-    Route::get('show/{id}', [CustomerController::class, 'show'])->name('customers.show');
-    Route::post('delete/{id}', [CustomerController::class, 'delete'])->name('customers.delete');
+        Route::get('', [CustomerController::class, 'index'])->name('customers.index');
+        Route::post('update', [CustomerController::class, 'update'])->name('customers.update');
+        Route::get('edit/{id}', [CustomerController::class, 'edit'])->name('customers.edit');
+        Route::get('show/{id}', [CustomerController::class, 'show'])->name('customers.show');
+        Route::post('delete/{id}', [CustomerController::class, 'delete'])->name('customers.delete');
+        Route::post('contact', [CustomerController::class, 'contact'])->name('customers.contact');
 
-});
+    });
 
-Route::group(['prefix' => 'test'], function () {
-    Route::get('', [TestController::class, 'index'])->name('test.index');
-    Route::get('update', [TestController::class, 'update'])->name('test.update');
-    Route::get('show/{id}', [TestController::class, 'show'])->name('test.show');
-    Route::post('delete/{id}', [TestController::class, 'delete'])->name('test.delete');
+    Route::group(['prefix' => 'test'], function () {
+        Route::get('', [TestController::class, 'index'])->name('test.index');
+        Route::get('update', [TestController::class, 'update'])->name('test.update');
+        Route::get('show/{id}', [TestController::class, 'show'])->name('test.show');
+        Route::post('delete/{id}', [TestController::class, 'delete'])->name('test.delete');
 
+    });
 });
