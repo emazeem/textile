9ce437eee17b6cdbd5bd9e2d5119514189b6c677
Ahmad <95245638+Ahmaddar007@users.noreply.github.com>
2024-04-25 15:20:52 +0500

    seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        // User::factory(10)->create();
+        $this->call([
+          PermissionSeeder::class
+        ]);
 
-        User::factory()->create();
+        // User::factory()->create();
     }
 }
--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 use App\Model\Admin\Permission;
+use App\Model\Admin\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,10 +13,56 @@ class PermissionSeeder extends Seeder
      */
     public function run(): void
     {
-        $permission = new Permissions;
-        $permission-> $user = "super-admin";
-        $permission-> $role = "";
-        $permission-> $designation = "";
-        $permission-> $department = "";
-    }
+        $adminRole = Role::where('name', 'admin')->first();
+        $adminRole->givePermissionTo('create user');
+        $adminRole->givePermissionTo('edit user');
+        $adminRole->givePermissionTo('delete user');
+        $adminRole->givePermissionTo('show user');
+        $adminRole->givePermissionTo('create customer');
+        $adminRole->givePermissionTo('edit customer');
+        $adminRole->givePermissionTo('delete customer');
+        $adminRole->givePermissionTo('show customer');
+        $adminRole->givePermissionTo('create test');
+        $adminRole->givePermissionTo('edit test');
+        $adminRole->givePermissionTo('delete test');
+        $adminRole->givePermissionTo('show test');
+        $adminRole->givePermissionTo('create range');
+        $adminRole->givePermissionTo('edit range');
+        $adminRole->givePermissionTo('delete range');
+        $adminRole->givePermissionTo('show range');
+        $adminRole->givePermissionTo('create department');
+        $adminRole->givePermissionTo('edit department');
+        $adminRole->givePermissionTo('delete department');
+        $adminRole->givePermissionTo('create designation');
+        $adminRole->givePermissionTo('edit designation');
+        $adminRole->givePermissionTo('delete desgination');
+        $adminRole->givePermissionTo('create work-order');
+        $adminRole->givePermissionTo('edit work-order');
+        $adminRole->givePermissionTo('delete work-order');
+        $adminRole->givePermissionTo('show work-order');  
+        $adminRole->givePermissionTo('create permission');
+        $adminRole->givePermissionTo('edit permission');
+        $adminRole->givePermissionTo('delete permission');
+        $adminRole->givePermissionTo('show permission');
+        $adminRole->givePermissionTo('create role');
+        $adminRole->givePermissionTo('edit role');
+        $adminRole->givePermissionTo('delete role');              
+
+
+        // $editorRole = Role::where('name', 'editor')->first();
+        // $editorRole->givePermissionTo('create posts');
+        // $editorRole->givePermissionTo('edit posts');
+        // $editorRole->givePermissionTo('delete posts');
+        // $editorRole->givePermissionTo('publish posts');
+        // $editorRole->givePermissionTo('create comments');
+        // $editorRole->givePermissionTo('edit comments');
+        // $editorRole->givePermissionTo('delete comments');
+
+        $userRole = Role::where('name', 'user')->first();
+        $userRole->givePermissionTo('');
+        $userRole->givePermissionTo('');
+        $userRole->givePermissionTo('');
+        $userRole->givePermissionTo('');
+}
+
 }
