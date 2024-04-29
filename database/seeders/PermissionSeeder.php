f21bda323f7bf362009f1ddf2bbeb1b422ed649d
Umerr-Bashir <112628338+Umerr-Bashir@users.noreply.github.com>
2024-04-29 11:13:47 +0500

    permission seeder fix

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -15,8 +15,40 @@ class PermissionSeeder extends Seeder
     public function run(): void
     {
 
-        $adminPermission = Permission::create([
-            'name' => 'Admin',
+        $userPermission = Permission::create([
+            'name' => 'Users',
+            'parent_id' => null
+        ]);
+        $customerPermission = Permission::create([
+            'name' => 'Customers',
+            'parent_id' => null
+        ]);
+        $testPermission = Permission::create([
+            'name' => 'Tests',
+            'parent_id' => null
+        ]);
+        $rangePermission = Permission::create([
+            'name' => 'Ranges',
+            'parent_id' => null
+        ]);
+        $departmentPermission = Permission::create([
+            'name' => 'Departments',
+            'parent_id' => null
+        ]);
+        $designationPermission = Permission::create([
+            'name' => 'Designations',
+            'parent_id' => null
+        ]);
+        $workorderPermission = Permission::create([
+            'name' => 'Work-Orders',
+            'parent_id' => null
+        ]);
+        $permissionPermission = Permission::create([
+            'name' => 'Permissions',
+            'parent_id' => null
+        ]);
+        $rolePermission = Permission::create([
+            'name' => 'Roles',
             'parent_id' => null
         ]);
         
@@ -24,139 +56,171 @@ class PermissionSeeder extends Seeder
         $permissions = [
         [
             'name' => 'View User',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $userPermission->id,
         ],
         [
             'name' => 'Create User',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $userPermission->id,
         ],
         [
             'name' => 'Edit User',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $userPermission->id,
         ],
         [
             'name' => 'Show User',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $userPermission->id,
         ],
         [
             'name' => 'Delete User',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $userPermission->id,
+        ],
+        [
+            'name' => 'View Customer',
+            'parent_id' => $customerPermission->id,
         ],
         [
             'name' => 'Create Customer',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $customerPermission->id,
         ],
         [
             'name' => 'Edit Customer',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $customerPermission->id,
         ],
         [
             'name' => 'Delete Customer',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $customerPermission->id,
         ],
         [
             'name' => 'Show Customer',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $customerPermission->id,
+        ],
+        [
+            'name' => 'View Test',
+            'parent_id' => $testPermission->id,
         ],
         [
             'name' => 'Create Test',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $testPermission->id,
         ],
         [
             'name' => 'Edit Test',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $testPermission->id,
         ],
         [
             'name' => 'Delete Test',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $testPermission->id,
         ],
         [
             'name' => 'Show Test',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $testPermission->id,
+        ],
+        [
+            'name' => 'View Range',
+            'parent_id' => $rangePermission->id,
         ],
         [
             'name' => 'Create Range',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $rangePermission->id,
         ],
         [
             'name' => 'Edit Range',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $rangePermission->id,
         ],
         [
             'name' => 'Delete Range',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $rangePermission->id,
         ],
         [
             'name' => 'Show Range',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $rangePermission->id,
+        ],
+        [
+            'name' => 'View Department',
+            'parent_id' => $departmentPermission->id,
         ],
         [
             'name' => 'Create Department',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $departmentPermission->id,
         ],
         [
             'name' => 'Edit Department',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $departmentPermission->id,
         ],
         [
             'name' => 'Delete Department',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $departmentPermission->id,
+        ],
+        [
+            'name' => 'View Designation',
+            'parent_id' => $designationPermission->id,
         ],
         [
             'name' => 'Create Designation',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $designationPermission->id,
         ],
         [
             'name' => 'Edit Designation',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $designationPermission->id,
         ],
         [
             'name' => 'Delete Designation',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $designationPermission->id,
+        ],
+        [
+            'name' => 'View Work-Order',
+            'parent_id' => $workorderPermission->id,
         ],
         [
             'name' => 'Create Work-Order',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $workorderPermission->id,
         ],
         [
             'name' => 'Edit Work-Order',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $workorderPermission->id,
         ],
         [
             'name' => 'Delete Work-Order',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $workorderPermission->id,
         ],
         [
             'name' => 'Show Work-Order',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $workorderPermission->id,
+        ],
+        [
+            'name' => 'View Permission',
+            'parent_id' => $permissionPermission->id,
         ],
         [
             'name' => 'Create Permission',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $permissionPermission->id,
         ],
         [
             'name' => 'Edit Permission',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $permissionPermission->id,
         ],
         [
             'name' => 'Delete Permission',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $permissionPermission->id,
         ],
         [
             'name' => 'Show Permission',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $permissionPermission->id,
+        ],
+        [
+            'name' => 'View Role',
+            'parent_id' => $rolePermission->id,
         ],
         [
             'name' => 'Create Role',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $rolePermission->id,
         ],
         [
             'name' => 'Edit Role',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $rolePermission->id,
         ],
         [
             'name' => 'Delete Role',
-            'parent_id' => $adminPermission->id,
+            'parent_id' => $rolePermission->id,
         ]];
         DB::table('permissions')->insert($permissions);
     }
--- resources/views/admin/roles/create.blade.php
+++ resources/views/admin/roles/create.blade.php
@@ -1,22 +1,23 @@
 @extends('admin.layout.master')
 @section('content')
-    <script src="{{url('assets/js/1.10.1/jquery.min.js')}}"></script>
-    <script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3/jquery.inputmask.bundle.js"></script>
-    @if(Session::has('success'))
-    <script>
-    $(document).ready(function () {
-        swal("Done!", '{{Session('success')}}', "success");
-    });
-    </script>
-    @endif
-    <div class="row pb-3">
-        <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="">Roles List</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Add Details</li>
-            </ol>
-        </nav>
-        <form  action="{{route('roles.store')}}" style="width:100%" method="post" >
+<script src="{{url('assets/js/1.10.1/jquery.min.js')}}"></script>
+<script src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3/jquery.inputmask.bundle.js"></script>
+@if(Session::has('success'))
+<script>
+$(document).ready(function() {
+    swal("Done!", '{{Session('
+        success ')}}', "success");
+});
+</script>
+@endif
+<div class="row pb-3">
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="">Roles List</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Add Details</li>
+        </ol>
+    </nav>
+    <form action="{{route('roles.store')}}" style="width:100%" method="post">
         @csrf
         <div class="card">
             <div class="card-footer bg-white border-top">
@@ -30,34 +31,38 @@
                         <h3 class="font-weight-light">Permissions</h3>
                         <div class="my-4">
                             @foreach($parents as $parent)
-                            <h6 class="my-3"><i class="bx bxs-cog mr-1"></i>{{$parent->name}} Management</h6>
+                            <div>
+                            <input class="form-check-input ml-1" type="checkbox" name="child[]">
+                            <h6 class="my-3 ml-4"><i class="bx bxs-cog mr-1"></i>{{$parent->name}} Management</h6>
+                            </div>
                             @foreach($parent->child as $permission)
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="checkbox" name="child[]" value="{{$permission->id}}" id="permission_{{$permission->id}}">
+                               
+                                <input class="form-check-input" type="checkbox" name="child[]"
+                                    value="{{$permission->id}}" id="permission_{{$permission->id}}">
                                 <label class="form-check-label mr-3" for="permission_{{$permission->id}}">
                                     {{$permission->name}}
                                 </label>
-                             </div>
-                            @endforeach
-                            @endforeach
                             </div>
+                            @endforeach
+                            @endforeach
                         </div>
                     </div>
+                </div>
 
-                        
-                    </div>
-                      <div class="card-footer bg-white border-top">
-                        <div class="row">
-                            <div class="col-12">
-                                <button type="submit" class="btn btn-primary user-btn float-right">
-                                    <i class="feather icon-save"> </i> Save</button>
-                            </div>
-                        </div>
-                      </div>
             </div>
-        </div> 
-        </form>
-    </div>
+            <div class="card-footer bg-white border-top">
+                <div class="row">
+                    <div class="col-12">
+                        <button type="submit" class="btn btn-primary user-btn float-right">
+                            <i class="feather icon-save"> </i> Save</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+</form>
+</div>
 <style>
 .select2-container--default .select2-selection--multiple .select2-selection__choice {
     background-color: #27458c !important;
@@ -65,13 +70,13 @@
 }
 </style>
 <script>
-    $(document).ready(function() {
+$(document).ready(function() {
     $('.multi-select').select2();
 });
 </script>
 <div>
     <div>
-</div>
+    </div>
 </div>
 
 @endsection
